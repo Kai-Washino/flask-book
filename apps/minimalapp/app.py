be30cc7c098b43ae360bca423af599554a979ee4
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-07-25 01:00:58 +0900

    Add contact form

--- apps/minimalapp/app.py
+++ apps/minimalapp/app.py
@@ -1,5 +1,4 @@
-from flask import Flask, render_template, url_for
-
+from flask import Flask, current_app, g, render_template, url_for, request, redirect
 app = Flask(__name__)
 
 @app.route("/", methods=["GET", "POST"])
@@ -17,7 +16,22 @@ def show_name(name):
     return render_template("index.html",
                            name=name)
 
+@app.route("/contact")
+def contact():
+    return render_template("contact.html")
+
+@app.route("/contact/complete", methods = ["GET", "POST"])
+def contact_complete():
+    if request.method == "POST":
+        username = request.form["username"]
+        email = request.form["email"]
+        description = request.form["description"]        
+        return redirect(url_for("contact_complete"))
+    
+    return render_template("contact_complete.html")
+
 with app.test_request_context():
     print(url_for("index"))
     print(url_for("hello-endpoint", name="world"))
     print(url_for("show_name", name="ichiro", page="1")) # このpageはフロントからバックへ追加の情報を渡すイメージ
+
