b5084c7f0b01f078d891b267305ce841f268b9d1
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-08-01 18:38:48 +0900

    Update config

--- apps/app.py
+++ apps/app.py
@@ -3,20 +3,15 @@ from flask import Flask
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
+from apps.config import config
 
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
-def create_app():
+def create_app(config_key):
     app = Flask(__name__)
-
-    app.config.from_mapping(        
-        SECRET_KEY = "akjdjfaoiewjkfawjk",
-        SQLALCHEMY_DATABASE_URI=f"sqlite:///{Path(__file__).parent.parent / 'local.sqlite'}",
-        SQLALCHEMY_TRACK_MODIFICATIONS=False,
-        SQLALCHEMY_ECHO = True,
-        WTF_CSRF_SECRET_KEY="akjjalkejakjdkwasj",
-    )
+    app.config.from_object(config[config_key])
+    
     db.init_app(app)
     Migrate(app, db)
     csrf.init_app(app)
