c9c220d577007f0ba3473817882b40c92ea5224c
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-08-05 10:13:08 +0900

    Add auth form

--- apps/app.py
+++ apps/app.py
@@ -1,5 +1,6 @@
 from pathlib import Path
 from flask import Flask
+from flask_login import LoginManager
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from flask_wtf.csrf import CSRFProtect
@@ -8,6 +9,10 @@ from apps.config import config
 db = SQLAlchemy()
 csrf = CSRFProtect()
 
+login_manager = LoginManager()
+login_manager.login_view = "auth.signup"
+login_manager.login_message = ""
+
 def create_app(config_key):
     app = Flask(__name__)
     app.config.from_object(config[config_key])
@@ -15,6 +20,9 @@ def create_app(config_key):
     db.init_app(app)
     Migrate(app, db)
     csrf.init_app(app)
+
+    login_manager.init_app(app)
+
     from apps.crud import views as crud_views
     app.register_blueprint(crud_views.crud, url_prefix="/crud")
 
