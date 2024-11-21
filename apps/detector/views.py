1f6fb0fa0be38254191dedf8097cf57bc2d03166
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-11-21 22:59:16 +0900

    Fix detector view

--- apps/detector/views.py
+++ apps/detector/views.py
@@ -12,7 +12,10 @@ dt = Blueprint(
 @dt.route("/")
 def index():
     user_images = (
-        db.session.query(User, UserImage).join(UserImage).filter(User.id == UserImage.user_id).all()
+        db.session.query(User, UserImage)
+        .join(UserImage)
+        .filter(User.id == UserImage.user_id)
+        .all()
     )
     return render_template("detector/index.html", user_images=user_images)
 
