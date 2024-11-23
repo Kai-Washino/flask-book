5aa89ed88fc9b68098d206427dd727e0d239eaca
Kai Washino <79495772+Kai-Washino@users.noreply.github.com>
2024-11-23 18:14:24 +0900

    Add upload image

--- apps/detector/models.py
+++ apps/detector/models.py
@@ -8,6 +8,6 @@ class UserImage(db.Model):
     user_id = db.Column(db.String, db.ForeignKey("users.id"))
     image_path = db.Column(db.String)
     is_detected = db.Column(db.Boolean, default=False)
-    created_at = db.Column(db.DateTime, default=False)
+    created_at = db.Column(db.DateTime, default=datetime.now)
     updated_at = db.Column(db.DateTime, default=datetime.now, onupdate=datetime.now)
     
